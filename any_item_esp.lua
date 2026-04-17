775221d3e1c578bd1535ae34a2f92f698e135860
Jake <hukarijake@gmail.com>
2026-04-17 11:25:01 -0600

    Update any_item_esp.lua

--- any_item_esp.lua
+++ any_item_esp.lua
@@ -98,9 +98,11 @@ local GAME_TEMPLATES = {
 		{Path = "Items.InventoryBags", Color = Color3.fromRGB(255, 255, 255), TrackAll = true},
 		{Path = "Items.Items.Diamond", Color = Color3.fromRGB(255, 0, 255)}
 	},
-	["3107097964"] = { -- NTRTY - DWNTBNK
+	["3107097964"] = { -- NTRTY - DWNTWN_BNK
+		{Path = "Citizens", Color = Color3.fromRGB(255, 192, 203)},
 		{Path = "Police", Color = Color3.fromRGB(0, 0, 255)},
-		{Path = "Citizens", Color = Color3.fromRGB(255, 192, 203)}
+		{Path = "Map.KeyCard", Color = Color3.fromRGB(255, 165, 0), Rainbow = true},
+		{Path = "Cameras", Color = Color3.fromRGB(255, 0, 0)}
 	},
 	["1213821265"] = { -- NTRTY - RNB
 		{Path = "Citizens", Color = Color3.fromRGB(255, 192, 203)},
