f5b70950e7fa102d53c713a5e87d09a34c7c5216
Jake <hukarijake@gmail.com>
2026-04-17 11:51:12 -0600

    Update any_item_esp.lua

--- any_item_esp.lua
+++ any_item_esp.lua
@@ -107,6 +107,7 @@ local GAME_TEMPLATES = {
 	["1242009557"] = { -- NTRTY - BRCK_BNK
 		{Path = "Citizens", Color = Color3.fromRGB(255, 192, 203)},
 		{Path = "Police", Color = Color3.fromRGB(0, 0, 255)},
+		{Path = "Map.KeyCard", Color = Color3.fromRGB(255, 165, 0), Rainbow = true},
 		{Path = "Cameras", Color = Color3.fromRGB(255, 0, 0)}
 	},
 	["1213821265"] = { -- NTRTY - RNB
