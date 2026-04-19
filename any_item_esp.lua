cbf236a146cf86eaf504025ddb60db8224fd0559
Jake <hukarijake@gmail.com>
2026-04-19 09:41:17 -0600

    Update any_item_esp.lua

--- any_item_esp.lua
+++ any_item_esp.lua
@@ -110,7 +110,7 @@ local GAME_TEMPLATES = {
 	},
 	["1242009557"] = { -- NTRTY - BRCK_BNK
 		{Path = "Citizens", Color = Color3.fromRGB(255, 192, 203)},
-		{Path = "Police", Color = Color3.fromRGB(0, 255, 255), TrackedProperties = {Type = true, Health = true, Variant = true, RNGGroup = true}},
+		{Path = "Police", Color = Color3.fromRGB(0, 147, 255), TrackedProperties = {RNGGroup = true, Health = true, Variant = true}},
 		{Path = "Map.KeyCard", Color = Color3.fromRGB(255, 165, 0), Rainbow = true},
 		{Path = "Cameras", Color = Color3.fromRGB(255, 0, 0)}
 	},
