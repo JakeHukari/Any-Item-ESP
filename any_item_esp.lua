090a8123a44da29261a5ed5c87030d8b4697cc39
Jake <hukarijake@gmail.com>
2026-04-19 00:25:57 -0600

    Update any_item_esp.lua

--- any_item_esp.lua
+++ any_item_esp.lua
@@ -124,7 +124,7 @@ local GAME_TEMPLATES = {
 	["2088934656"] = { -- MTRTY - SHDOW
 		{Path = "Citizens", Color = Color3.fromRGB(255, 255, 255)},
 		{Path = "Map.EasterEggCrystal", Color = Color3.fromRGB(255, 255, 255), Rainbow = true},
-		{Path = "Police", Color = Color3.fromRGB(0, 255, 255), TrackedProperties = {RNGGroup = true, Type = true, Health = true, Variant = true}},
+		{Path = "Police", Color = Color3.fromRGB(0, 255, 255), TrackedProperties = {RNGGroup = true, Variant = true}},
 		{Path = "Lootables", Color = Color3.fromRGB(255, 165, 0)},
 		{Path = "BigLoot", Color = Color3.fromRGB(255, 255, 255), Rainbow = true},
 		{Path = "Map.ShadowBoxes", Color = Color3.fromRGB(255, 165, 0)}
