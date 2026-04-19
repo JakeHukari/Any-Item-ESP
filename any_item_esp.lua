0e2c03900d4684f5a6ed2b9c82ead3e142c34d7a
Jake <hukarijake@gmail.com>
2026-04-19 08:59:20 -0600

    Update any_item_esp.lua

--- any_item_esp.lua
+++ any_item_esp.lua
@@ -121,13 +121,18 @@ local GAME_TEMPLATES = {
 		{Path = "Citizens", Color = Color3.fromRGB(255, 192, 203)},
 		{Path = "Cameras", Color = Color3.fromRGB(255, 0, 0)}
 	},
-	["2088934656"] = { -- MTRTY - SHDOW
-		{Path = "Citizens", Color = Color3.fromRGB(255, 255, 255)},
+	["2088934656"] = { -- NTRTY - SHDOW
 		{Path = "Map.EasterEggCrystal", Color = Color3.fromRGB(255, 255, 255), Rainbow = true},
+		{Path = "Map.EasterEgg", Color = Color3.fromRGB(255, 255, 255), Rainbow = true},
+		{Path = "Map.ShadowBoxes", Color = Color3.fromRGB(255, 165, 0)},
 		{Path = "Police", Color = Color3.fromRGB(0, 255, 255), TrackedProperties = {RNGGroup = true, Variant = true}},
+		{Path = "BigLoot", Color = Color3.fromRGB(255, 165, 0)},
+		{Path = "Map.EasterEggBlueBook", Color = Color3.fromRGB(255, 255, 255), Rainbow = true},
+		{Path = "Map.EasterEggCrystal", Color = Color3.fromRGB(255, 255, 255), Rainbow = true},
 		{Path = "Lootables", Color = Color3.fromRGB(255, 165, 0)},
-		{Path = "BigLoot", Color = Color3.fromRGB(255, 255, 255), Rainbow = true},
-		{Path = "Map.ShadowBoxes", Color = Color3.fromRGB(255, 165, 0)}
+		{Path = "Map.EasterEggCrystal", Color = Color3.fromRGB(255, 255, 255), Rainbow = true},
+		{Path = "Map.EasterEggCrystal", Color = Color3.fromRGB(255, 255, 255), Rainbow = true},
+		{Path = "Citizens", Color = Color3.fromRGB(255, 255, 255)}
 	},
 	["1213821265"] = { -- NTRTY - RNB
 		{Path = "Citizens", Color = Color3.fromRGB(255, 192, 203)},
